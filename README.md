ad5f122e3c205b71a872276cc224d92a1f619d78
Tostito <tostitogaming@live.com>
2014-11-09 13:33:36 -0600

    initial commits
    initial commits

---
+++ README.md
@@ -0,0 +1,23 @@
+SE-CleanNames
+================
+
+Bad player and object names removal / rename
+
+**INTENDED FOR DEDICATED SERVERS
+*This script was designed to eliminate explosive save growth due to the special characters bug.
+
+If you intend to use these for commercial panel / virtual servers / personal use, go ahead. I just ask that you give me credit for it or leave a testimony of how it has benefited you here: http://forums.keenswh.com/post?id=7099011
+
+Thank you!
+
+The author of this script is not responsible for any damage to the game save or the machine it is run on.
+
+Bad player and object names removal / rename
+
+This powershell script is used by changing the variables at the top of the 'ps1' file to match your server settings. 
+
+The default settings will delete players of which their Player name exceeds 50 characters in length (due to a special characters bug. Custom object names that have over 300 characters will be reset to "Name corrupted and reset"
+
+Be sure to shut your space engineers server down before running this script!!!
+
+Use the windows shortcut provided to run. Be sure the shortcut and the script file are in the same folder.
---
+++ cleannames.ps1
@@ -0,0 +1,57 @@
+﻿   
+   $badIDlogs = "yourlogspathhere \Admin Logs\Audits\deleted\"
+   $badNAMElogs = "yourlogspathhere \Admin Logs\Audits\deleted\"
+   $filePath = 'yoursavepathhere \SANDBOX_0_0_0_.sbs'
+   $filePath2 = 'yoursavepathhere \SANDBOX.sbc'
+   
+   #you only need to change the above paths
+
+   $CurrentDateTime = Get-Date -Format "MM-dd-yyyy_HH-mm"
+   $badIDfilename = "BadPlayerID_Audit_" +$CurrentDateTime+ ".log"
+   $badIDpath = $badIDLogs + $badIDfilename
+
+   $badNAMEfilename = "Badblockname_Audit_" +$CurrentDateTime+ ".log"
+   $badNAMEpath = $badNAMELogs + $badNAMEfilename
+
+   New-Item -path $badIDpath -type file
+   New-Item -path $badNAMEpath -type file
+   
+   #$filePath = 'F:\DedicatedServer\DataDir\VPS Dedicated 1\Saves\Sagittaron Sector\SANDBOX_0_0_0_.sbs'
+   $filePath = 'F:\DedicatedServer\DataDir\SE Survival 2\Saves\Sagittaron Sector\SANDBOX_0_0_0_.sbs'
+   #$filePath2 = 'F:\DedicatedServer\DataDir\VPS Dedicated 1\Saves\Sagittaron Sector\SANDBOX.sbc'
+   $filePath2 = 'F:\DedicatedServer\DataDir\SE Survival 2\Saves\Sagittaron Sector\SANDBOX.sbc'
+
+   [xml]$myXML = Get-Content $filePath
+   $ns = New-Object System.Xml.XmlNamespaceManager($myXML.NameTable)
+   $ns.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+   [xml]$myXML2 = Get-Content $filePath2
+   $ns2 = New-Object System.Xml.XmlNamespaceManager($myXML2.NameTable)
+   $ns2.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+   Write-Host -ForegroundColor Green " Checking for bad player names ... "
+   $findinvalidplayer = $myXML2.SelectNodes("//Identities/MyObjectBuilder_Identity/DisplayName"  , $ns2)
+   ForEach($player in $findinvalidplayer){
+       IF($player.InnerText.Length -gt 50){
+            $player.InnerXml
+            Write-Host -ForegroundColor Green " Bad ID deleted. "
+            Add-Content -Path $badIDpath -Value "$($player.InnerXml)"
+            Add-Content -Path $badIDpath -Value "Bad ID deleted"
+            $Player.ParentNode.ParentNode.RemoveChild($Player.ParentNode)
+       }
+   }
+
+   Write-Host -ForegroundColor Green " Checking for bad Display Names ... "
+   $findinvalidDN = $myXML.SelectNodes("//SectorObjects/MyObjectBuilder_EntityBase/CubeBlocks/MyObjectBuilder_CubeBlock/DisplayName | //SectorObjects/MyObjectBuilder_EntityBase/CubeBlocks/MyObjectBuilder_CubeBlock/CustomName" ,$ns)
+   ForEach($name in $findinvalidDN){
+       IF($name.InnerText.Length -gt 300){
+            $name.InnerXml
+            Write-Host -ForegroundColor Green " Bad Name detected. Name was reset."
+            Add-Content -Path $badNAMEpath -Value "$($name.InnerXml)"
+            Add-Content -Path $badNAMEpath -Value "Bad Name detected. Name was reset."
+            $name.InnerXML = "Name corrupted and reset"
+       }
+   }
+
+$myXML2.Save($filePath2)
+$myXML.Save($filePath)
